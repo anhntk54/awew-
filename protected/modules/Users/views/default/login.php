62a9e0ac31ae02ca4f8b0632135f59f08177d62b
anhntk54 <anhntk54@gmail.com>
2014-03-18 01:56:57 +0700

    view login

--- protected/modules/Users/views/default/login.php
+++ protected/modules/Users/views/default/login.php
@@ -7,58 +7,39 @@
 	<div class="form">
 	<?php $form=$this->beginWidget('CActiveForm', array(
 		'id'=>'login-form',
-		// 'enableClientValidation'=>true,
-		// 'clientOptions'=>array(
-		// 	'validateOnSubmit'=>true,
-		// ),
+		'enableClientValidation'=>true,
+		'clientOptions'=>array(
+			'validateOnSubmit'=>true,
+		),
 	)); ?>
 	<?php echo $form->errorSummary($model); ?>
 		<div class="row">
-			<?php echo $form->labelEx($model,'username'); ?>
-			<?php echo $form->textField($model,'username',array('class'=>'input-login')); ?>
+			<p><?php echo $model->getAttributeLabel('username'); ?></p>
+			<?php echo $form->textField($model,'username',array('class'=>'input-login-view')); ?>
 			<?php echo $form->error($model,'username'); ?>
 		</div>
 
 		<div class="row">
-			<?php echo $form->labelEx($model,'password'); ?>
-			<?php echo $form->passwordField($model,'password',array('class'=>'input-login')); ?>
+			<p><?php echo $model->getAttributeLabel('password'); ?></p>
+			<?php echo $form->passwordField($model,'password',array('class'=>'input-login-view')); ?>
 			<?php echo $form->error($model,'password'); ?>
 		</div>
 
-		<div class="row rememberMe">
-			<?php echo $form->checkBox($model,'rememberMe'); ?>
-			<?php echo $form->label($model,'rememberMe'); ?>
-			<?php echo $form->error($model,'rememberMe'); ?>
-		</div>
+		<div class="login-bottom">
+			<div class="row rememberMe">
+				<?php echo $form->checkBox($model,'rememberMe'); ?>
+				<?php echo $form->label($model,'rememberMe'); ?>
+				<?php echo $form->error($model,'rememberMe'); ?>
+			</div>
 
-		<div class="row buttons">
-			<?php echo CHtml::submitButton('Login'); ?>
+			<div class="row buttons">
+				<?php echo CHtml::submitButton('Đăng nhập',array('class'=>'login-submit')); ?>
+				<p>Hoặc </p><a href="">Đăng ký tài khoản</a>
+			</div>
+			<a href="">Quên mật khẩu?</a>
 		</div>
 
 	<?php $this->endWidget(); ?>
 	</div><!-- form -->
 </div>
-<style type="text/css">
-	.login h5{
-		font-weight: bold;
-		color: #000;
-	}
-	.row label,.row .input-login{
-		display: inline-block;
-	}
-	.row .input-login{
-		width: 30%;
-	}
-	.row input{}
-	.login{
-		width: 50%;
-		margin: 0px auto;
-		border: 1px solid #857D7D;
-		border-radius: 6px;
-		padding: 16px;
-	}
-	.login-main{
-		width: 100%;
-	}
-</style>
 </section>
