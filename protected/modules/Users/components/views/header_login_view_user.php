135662cff8f8dfb7fd1b31e0bedcfe32663ea0f4
anhntk54 <anhntk54@gmail.com>
2014-03-21 01:36:47 +0700

    css avatar

--- protected/modules/Users/components/views/header_login_view_user.php
+++ protected/modules/Users/components/views/header_login_view_user.php
@@ -8,29 +8,31 @@
 	</div>
 	<div class="activity-user">
 		<span class="icon"></span>
+		<div></div>
+		<ul class="activity">
+			<li><a href="<?php echo Users::getUrlUser(); ?>">Trang cá nhân</a></li>
+			<li><a href="">Trang fanpage</a></li>
+			<li><a href="">Cài đặt</a></li>
+		</ul>
 	</div>
 </div>
-<ul class="activity">
-	<li><a href="<?php echo Users::getUrlUser(); ?>">Trang cá nhân</a></li>
-	<li><a href="">Trang fanpage</a></li>
-	<li><a href="">Cài đặt</a></li>
-</ul>
+
 <script type="text/javascript">
-	var showUl = 0;
-	jQuery('.activity-user').hover(function() {
-		jQuery('.activity').fadeIn();
-	},function(e) {
-		console.log(showUl);
-		setTimeout(function() {
-			if(showUl == 0){
-				jQuery('.activity').fadeOut();
-			}
-		},500);
-	});
-	jQuery('.activity').hover(function() {
-		showUl = 1;
-	},function() {
-		showUl = 0;
-		jQuery('.activity').fadeOut();
-	});
+	// var showUl = 0;
+	// jQuery('.activity-user').hover(function() {
+	// 	jQuery('.activity').fadeIn();
+	// },function(e) {
+	// 	console.log(showUl);
+	// 	setTimeout(function() {
+	// 		if(showUl == 0){
+	// 			jQuery('.activity').fadeOut();
+	// 		}
+	// 	},500);
+	// });
+	// jQuery('.activity').hover(function() {
+	// 	showUl = 1;
+	// },function() {
+	// 	showUl = 0;
+	// 	jQuery('.activity').fadeOut();
+	// });
 </script>
