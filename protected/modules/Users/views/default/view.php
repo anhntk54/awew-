9fadead6e1b8afff6e1a3c8ee893f9fb6b825105
Tuấn anh <anhntk54@gmail.com>
2014-03-25 17:41:36 +0700

    sidebar view

--- protected/modules/Users/views/default/view.php
+++ protected/modules/Users/views/default/view.php
@@ -1,5 +1,6 @@
 <section id="main" class="prl-span-9">
 	<?php $this->widget('Users.components.View_User_Thumbai',array('model'=>$model)); ?>
+	<?php $this->widget('Users.components.View_Left_Sidebar'); ?>
 	<div class="post-status-user">
 		<div contenteditable="true" class="input" data-ph="Bạn đang nghĩ gì?"></div>
 		<i class="fa fa-camera"></i>
