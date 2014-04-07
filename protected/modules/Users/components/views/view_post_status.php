69834d9fcaa1e1bc7b71d710b2f302e54756c9b7
Tuấn anh <anhntk54@gmail.com>
2014-04-07 20:24:57 +0700

    view

--- protected/modules/Users/components/views/view_post_status.php
+++ protected/modules/Users/components/views/view_post_status.php
@@ -14,26 +14,5 @@
 	<div class="clr"></div>
 </div>
 <script type="text/javascript">
-	var txt = jQuery('#comments'),
-    hiddenDiv = jQuery(document.createElement('div')),
-    content = null;
-
-	txt.addClass('txtstuff');
-	hiddenDiv.addClass('hiddendiv common');
-
-	jQuery('body').append(hiddenDiv);
-
-	txt.on('keyup', function () {
-
-	    content = jQuery(this).val();
-
-	    content = content.replace(/\n/g, '<br>');
-	    hiddenDiv.html(content + '<br class="lbr">');
-	    height = hiddenDiv.height() > 40 ? hiddenDiv.height() : 40;
-	    jQuery(this).css('height', height);
-
-	});
-	jQuery('.status-post').click(function() {
-		jQuery('.types').slideToggle();
-	})
+	
 </script>
