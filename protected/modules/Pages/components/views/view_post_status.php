588c08825aeec5091ceaba2cdd14692545839a3f
tiennm54 <minhtienuet@gmail.com>
2014-04-22 17:15:18 +0700

    22-04

--- protected/modules/Pages/components/views/view_post_status.php
+++ protected/modules/Pages/components/views/view_post_status.php
@@ -2,7 +2,7 @@
 	<textarea id="input-comments" placeholder="Bạn đang nghĩ gì?"></textarea>
 	<div class="div-button">
 		<a href="javscript::void(0);"><i class="fa fa-camera"></i></a>
-		<button class="post-status" id="post-status-all">Đăng bài</button>
+		<button class="post-status" id="post-status-all" onclick="post_status();">Đăng bài</button>
 		<div class="status-post">
 			<a href="javscript::void(0);">Công khai</a>
 			<i class="fa fa-filter fa-lg"></i>
@@ -14,6 +14,18 @@
 	<div class="clr"></div>
 </div>
 <script type="text/javascript">
+
+    function post_status(){
+        jQuery.ajax({
+			url:'<?php echo Yii::app()->createUrl("/Status/default/create") ?>',
+			data:{c:content,t:'<?php echo $this->type; ?>'},
+			type:'POST',
+			success:function (data) {
+				jQuery('.posts-status').prepend(data);
+			},
+		});
+    }
+
 	jQuery('#post-status-all').click(function (e) {
 		console.log(jQuery('#input-comments').val());
 		var content = jQuery('#input-comments').val().replace(/\n/g, '<br />');
